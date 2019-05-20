d0861f95077b966032222cdeb126781138682b09
Andreas Waschnig <andreas.waschnig@gmail.com>
2019-05-20 10:09:10 -0400

    Add files via upload

---
+++ ConsoleAppT1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
